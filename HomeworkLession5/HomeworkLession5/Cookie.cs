10965c8ef55a990d94ab10b97c775bee49d0df81
Orel Mordechai <44357944+ORELxD@users.noreply.github.com>
2018-11-25 16:15:14 +0200

    CSHARP

---
+++ HomeworkLession5/HomeworkLession5/Cookie.cs
@@ -0,0 +1,62 @@
+﻿
+namespace HomeworkLession5
+{
+    class Cookie
+    {
+        // var gluten get+set
+        private bool gluten;
+
+        public bool Gluten
+        {
+            get { return gluten; }
+            set{
+                if(!gluten)
+                gluten =value;
+            }
+        }
+
+        // var eggs get+set
+        private int eggs;
+
+        public int Eggs
+        {
+            get { return eggs; }
+            set {
+                if(value>0)
+                eggs = value;
+            }
+        }
+
+        // var suger get+set
+        private double sugar;
+
+        public double Suger
+        {
+            get { return sugar; }
+            set
+            {
+                if (value > 0)
+                    sugar = value;
+            }
+        }
+
+        // var flour get+set
+        private double flour;
+
+        public double Flour
+        {
+            get { return flour; }
+            set {
+                if (value > 0)
+                    flour = value;
+            }
+        }
+
+        //Function that print the Descroption of Cookies
+        public string GetDescription()
+        {
+            return $"#Gluten- {Gluten}\n#Eggs- {Eggs}\n#Suger- {Suger}\n#Flour- {Flour}" ;
+        }
+
+    }
+}
---
+++ HomeworkLession5/HomeworkLession5/Program2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace HomeworkLession5
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            //Create new car
+            Car car1 = new Car();
+            //Show the info about the new car that i create
+            Console.WriteLine("************STEP-1**************");
+            Console.WriteLine(car1.GetDescription());
+            //ask the user how much he want to speed his car?
+            Console.WriteLine("How much kmh do you want speed the car?");
+            car1.Speed = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine($"~~~~Your speed is:{car1.Speed}-kmh ~~~~~\n");
+            Console.ReadLine();
+            // Show the info after the car slowdown by 2 kmh 
+            car1.Speed -= 2;
+            car1.SlowDown = 2;
+            Console.WriteLine("************STEP-2**************");
+            Console.ReadLine();
+            Console.WriteLine($"You slow your speed by:{car1.SlowDown}-kmh\n");
+            Console.WriteLine(car1.GetDescription());
+            // Show the info 
+            car1.Speed = 0;
+            car1.SlowDown = 0;
+            car1.Stop = true;
+            Console.ReadLine();
+            Console.WriteLine("************STEP-3**************");
+            Console.WriteLine("You stop the car!!");
+            Console.WriteLine(car1.GetDescription());
+        }
+    }
+}
